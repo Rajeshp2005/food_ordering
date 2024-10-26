cfe3c41ce93da0506e6c13e45942fcd7b9d75fab
rajeshvai <imrajesh2005@gmail.com>
2024-10-26 20:30:35 +0545

    first-commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const closeIcon = document.querySelector('.close-icon');
 const hamburgerMenu = document.querySelector('.hamburger-menu-container');
 const headerContent = document.querySelector('.header-content');
-hamburgerMenu.addEventListener('click',()=>{
+hamburgerMenu.addEventListener('click',(e)=>{
+  e.stopPropagation()
    headerContent.classList.add('menu-open');
 })
 closeIcon.addEventListener('click',()=>{
+  headerContent.classList.remove('menu-open');
+})
+window.addEventListener('click',()=>{
   headerContent.classList.remove('menu-open');
 })
 
